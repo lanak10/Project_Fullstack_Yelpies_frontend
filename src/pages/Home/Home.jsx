b15708967add2a9a4e21087ed6a9666216fb0a95
Lana Kim <lanakim@Lanas-MacBook-Pro.local>
2022-07-12 23:45:50 -0400

    installed react-icons

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,18 +4,22 @@ const Home = () => {
     return (
         <div>
             <img src="https://www.bakemag.com/ext/resources/images/2019/2/FlourBakery_StickyStickyBuns.jpg?t=1549291496&width=1080" className="homeImg"></img>
-            <div class="card">
-                <h3 class="card-title">Categories</h3>
-                <div id="category-cards">
-                    <div class="card">Restaurants</div>
-                    <div class="card">Shopping</div>
-                    <div class="card">Night Life</div>
-                    <div class="card">Beauty & Spas</div>
-                    <div class="card">Automotive</div>
-                    <div class="card">Home Services</div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3 class="card-title">Categories</h3>
+                                <div class="card">Restaurants</div>
+                                <div class="card">Shopping</div>
+                                <div class="card">Night Life</div>
+                                <div class="card">Beauty & Spas</div>
+                                <div class="card">Automotive</div>
+                                <div class="card">Home Services</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
     );
 }
 
